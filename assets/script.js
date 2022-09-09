992535e440552030bb6e6a9663c06b502935530a
Savannah McGinnis <sjmhorsegirl@verizon.net>
2022-09-09 14:12:19 -0500

    first commit

---
+++ assets/script.js
@@ -0,0 +1,35 @@
+let apiKey = ''
+
+let cityName = "Dallas";
+fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${cityName}&appid=${apiKey}`)
+.then(function (response) {
+
+    return response.json()
+
+})
+.then(function (cityData){
+
+
+    console.log(cityData[0].lat);
+    console.log(cityData[0].lon);
+
+    return {lat:cityData[0].lat, lon: cityData[0].lon}
+
+})
+.then(function(latLonData){
+    return fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${latLonData.lat}&lon=${latLonData.lon}&appid=${apiKey}`)
+        .then(function (response) {
+    
+            return response.json()
+    
+        })
+        .then(function (data) {
+    
+            console.log(data);
+    
+    
+        })
+
+
+
+})
