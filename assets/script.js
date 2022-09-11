12e280dbd05420124a8b1f7afbfa300a9ad47d82
Savannah McGinnis <sjmhorsegirl@verizon.net>
2022-09-11 15:38:32 -0500

    more data added

--- assets/script.js
+++ assets/script.js
@@ -1,23 +1,14 @@
 let apiKey = '8f8c6da20ce2891419f3a9757fa8ba62';
 let cityInput = document.querySelector("#cityInput");
 let searchBtn = document.querySelector("#searchBtn");
-let currentDate = moment().format(MM/DD/YYYY);
+// let currentDate = moment().format(MM/DD/YYYY);
 
-document.querySelector("#currentDate").textContent = currentDate;
+// document.querySelector("#currentDate").textContent = currentDate;
 
-// let displayCity = document.querySelector("#displayCity");
-// let currentDate = moment().
-// let cityName = cityInput.value;
-
-// searchBtn.addEventListener('click', function(){
-//     if (cityName !== null){
-//         citySearch();
-//     }
-// });
 
 let cityName = "Dallas";
 
-// function citySearch(){
+
     
     fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${cityName}&appid=${apiKey}`)
     .then(function (response) {
@@ -49,11 +40,24 @@ let cityName = "Dallas";
             document.querySelector("#currentTemp").textContent = ("Temp: " + data.list[0].main.temp);
             document.querySelector("#currentWind").textContent = ("Wind: " + data.list[0].wind.speed);
             document.querySelector("#currentHumidity").textContent = ("Humidity: " + data.list[0].main.humidity);
+            document.querySelector("#next1Temp").textContent = ("Temp: " + data.list[1].main.temp);
+            document.querySelector("#next1Wind").textContent = ("Wind: " + data.list[1].wind.speed);
+            document.querySelector("#next1Humidity").textContent = ("Humidity: " + data.list[1].main.humidity);
+            document.querySelector("#next2Temp").textContent = ("Temp: " + data.list[2].main.temp);
+            document.querySelector("#next2Wind").textContent = ("Wind: " + data.list[2].wind.speed);
+            document.querySelector("#next2Humidity").textContent = ("Humidity: " + data.list[2].main.humidity);
+            document.querySelector("#next3Temp").textContent = ("Temp: " + data.list[3].main.temp);
+            document.querySelector("#next3Wind").textContent = ("Wind: " + data.list[3].wind.speed);
+            document.querySelector("#next3Humidity").textContent = ("Humidity: " + data.list[3].main.humidity);
+            document.querySelector("#next4Temp").textContent = ("Temp: " + data.list[4].main.temp);
+            document.querySelector("#next4Wind").textContent = ("Wind: " + data.list[4].wind.speed);
+            document.querySelector("#next4Humidity").textContent = ("Humidity: " + data.list[4].main.humidity);
+            document.querySelector("#next5Temp").textContent = ("Temp: " + data.list[5].main.temp);
+            document.querySelector("#next5Wind").textContent = ("Wind: " + data.list[5].wind.speed);
+            document.querySelector("#next5Humidity").textContent = ("Humidity: " + data.list[5].main.humidity);
+            
             
             
         })
     })
-// };
 
-
-// displayCity.textContent(cityName + )
